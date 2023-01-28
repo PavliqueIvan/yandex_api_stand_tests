16811754c764c03ed205ffa15d2305a7b8c2bb35
Your Name <you@example.com>
2023-01-28 19:13:04 +0700

    commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c3e34cc4-f264-42fb-8e2d-6b2366a53114.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
